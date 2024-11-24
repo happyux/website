e30d8f26a7978878e9ec9eb3f108b9ad964a6026
happyux <bno@happyux.ai>
2024-11-24 12:00:17 -0600

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,33 +1,9 @@
-# dependencies
-/node_modules
-/.pnp
-.pnp.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['placeholder.com'],
+  },
+}
 
-# testing
-/coverage
-
-# next.js
-/.next/
-/out/
-
-# production
-/build
-
-# misc
-.DS_Store
-*.pem
-
-# debug
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-
-# local env files
-.env*.local
-
-# vercel
-.vercel
-
-# typescript
-*.tsbuildinfo
-next-env.d.ts
+module.exports = nextConfig
